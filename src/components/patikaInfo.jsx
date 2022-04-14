5c5add7587a4b48323a8c550dae52881571b1c0c
NijatBakhtiyar <NijatBakhtiyar-iatc>
2022-04-14 18:11:26 +0400

    sadas

--- src/components/patikaInfo.jsx
+++ src/components/patikaInfo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-// import BigRhombusIcon from "../constant/icons/bigRhombusIcon";
+import BigRhombusIcon from "../constant/icons/testA";
 import PatikaIcon from "../constant/icons/patikaIcon";
-// import SmallRhombusIcon from "../constant/icons/smallRhombusIcon";
-// import { useAuthContex } from "../context/authContext";
-import LoadingIcon from "../constant/icons/loadingIcon";
+import SmallRhombusIcon from "../constant/icons/testB";
+import { useAuthContex } from "../context/authContext";
 
 function PatikaInfo() {
-    // const { mode } = useAuthContex();
+    const { mode } = useAuthContex();
 
     return (
         <div className="patika-info">
@@ -14,9 +13,8 @@ function PatikaInfo() {
             <h1>Yazılım Patikaları</h1>
             <p>Bootcamp`ler teknoloji kariyerine girmek isteyenler için yeni bir eğitim modelidir. Ekibini büyütmek isteyen şirketler bir bootcamp`lere sponsor olurlar. Teknik beceriler kazanmaya başlamış ancak işe girmeye hazır olmayan kişiler bootcamp`lere başvururlar. Seçilen adaylar 4-8 haftalık ücretsiz ve yoğun eğitime kabul alırlar. Bootcamp`lerde başarılı olan öğrenciler sponsor şirkette ya da sektörde başka şirketlere işe yerleşirler.</p>
             <div className="icons">
-                {/* <SmallRhombusIcon params={mode === "dark" && { color: "#FFBF5E" }} /> */}
-                {/* <BigRhombusIcon params={mode === "dark" && { color: "#FFBF5E" }} /> */}
-                <LoadingIcon/>
+                <SmallRhombusIcon params={mode === "dark" && { color: "#FFBF5E" }} />
+                <BigRhombusIcon params={mode === "dark" && { color: "#FFBF5E" }} />
             </div>
         </div>
     );
